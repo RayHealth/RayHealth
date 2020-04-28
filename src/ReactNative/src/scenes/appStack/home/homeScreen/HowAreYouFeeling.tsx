c0c56f89d328deb39b4e6a91e288bda331044556
kevin Horek <kevinhorek@gmail.com>
2020-04-27 23:00:04 -0600

    Adding in more theming

--- src/ReactNative/src/scenes/appStack/home/homeScreen/HowAreYouFeeling.tsx
+++ src/ReactNative/src/scenes/appStack/home/homeScreen/HowAreYouFeeling.tsx
@@ -1,10 +1,10 @@
 import React, {useCallback} from "react";
-import {BaseContainerView} from "../../styles";
+import {FeelingContainerView} from "../../styles";
 import {useDispatch} from "react-redux";
 import {initializeAssessment} from "@reduxShared/models/assessments/actions";
 import {useUuid} from "../../../utils/customHooks/useUuid";
 import {BrandFullWidthButton} from "../../../sharedComponents/buttons";
-import {DefaultText} from "../../../../config/styleDefaults";
+import {HowAreYouFeelingHeader} from "../../../../config/styleDefaults";
 
 const HowAreYouFeeling: React.FC = () => {
     const dispatch = useDispatch();
@@ -16,13 +16,13 @@ const HowAreYouFeeling: React.FC = () => {
         dispatch(initializeAssessment(uuid, false));
     }, [dispatch, uuid]);
     return (
-        <BaseContainerView>
-            <DefaultText>How are you feeling?</DefaultText>
+        <FeelingContainerView>
+            <HowAreYouFeelingHeader>How are you feeling?</HowAreYouFeelingHeader>
             <BrandFullWidthButton onPress={isFeelingGood}>Great</BrandFullWidthButton>
             <BrandFullWidthButton onPress={isNotFeelingGood}>
                 Not so great
             </BrandFullWidthButton>
-        </BaseContainerView>
+        </FeelingContainerView>
     );
 };
 export default HowAreYouFeeling;
--- src/ReactNative/src/scenes/appStack/styles.tsx
+++ src/ReactNative/src/scenes/appStack/styles.tsx
@@ -21,6 +21,20 @@ export const HeaderView = styled.View`
     height: 114px;
 `;
 
+export const FeelingContainerView = styled.View`
+    margin-top: 40px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    padding-top: 15px;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-bottom: 15px;
+    color: ${STYLE.COLORS.GREY1};
+    background: ${STYLE.COLORS.WHITE};
+    border-radius: ${STYLE.BORDER_RADIUS}px;
+`;
+
 export const BaseContainerView = styled.View`
     margin-top: 10px;
     margin-left: 10px;
